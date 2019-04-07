d98f87da9791e28b136540b63b301fb38a6569aa
qkrgusdn21 <37537310+qkrgusdn21@users.noreply.github.com>
2019-04-07 22:28:12 +0900

    view까지 만들어 봄

--- HUFS/HUFS/urls.py
+++ HUFS/HUFS/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import project.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', project.views.home, name = 'home'),
 ]
