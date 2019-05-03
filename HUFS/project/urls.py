5c8d6313b187d2c7eefb22f8c466ce33d09a1bf6
qkrgusdn21 <37537310+qkrgusdn21@users.noreply.github.com>
2019-05-03 10:17:14 +0900

    delete project

--- HUFS/project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name= "project"
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name = 'index'),
-    # /project 으로 접속시 (include 됨)
-   ]
