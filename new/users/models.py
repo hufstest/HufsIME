64b4263f9d5add66614f2fffb49244c8d1025306
daechul <44284694+KsPsD@users.noreply.github.com>
2019-05-07 09:41:33 +0900

    model

--- new/users/models.py
+++ new/users/models.py
@@ -8,3 +8,85 @@ class CustomUserManager(UserManager):
 
 class CustomUser(AbstractUser):
     objects = CustomUserManager()
+
+class article(models.Model):
+    content = models.TextField()
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,)
+
+    like_user_set = models.ManyToManyField(
+        CustomUser,
+        null=True,
+        blank=True,
+        related_name='like_user_set',
+        through='Like',
+    )
+
+    comment_user_set = models.ManyToManyField(
+        CustomUser,
+        null=True,
+        blank=True,
+        related_name='comment_user_set',
+        through= "comment"
+    )
+
+    answer_user_set = models.ManyToManyField(
+        CustomUser,
+        null=True,
+        blank=True,
+        related_name='answer_user_set',
+        through='answer',
+    )
+
+class Like(models.Model):
+
+    article = models.ForeignKey(
+        article, on_delete=models.CASCADE,
+    )
+
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+class comment(models.Model):
+    article = models.ForeignKey(
+        article, on_delete=models.CASCADE
+    )
+
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE,
+    )
+    comment_text=models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+class answer(models.Model):
+    article = models.ForeignKey(
+        article, on_delete=models.CASCADE,
+    )
+
+    user = models.ForeignKey(
+        CustomUser,on_delete=models.CASCADE,
+    )
+    answer_text = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
